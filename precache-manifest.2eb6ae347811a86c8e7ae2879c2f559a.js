2b185dd726933e2b3ff7ba5d72526717added69f
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-03-05 17:02:24 -0300

    Updates

---
+++ precache-manifest.2eb6ae347811a86c8e7ae2879c2f559a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fd668cdac5eda0bf90e",
+    "url": "/natural_pic/static/css/main.97e4c41e.chunk.css"
+  },
+  {
+    "revision": "0fd668cdac5eda0bf90e",
+    "url": "/natural_pic/static/js/main.efa68573.chunk.js"
+  },
+  {
+    "revision": "9eb4d3c80145301ba1f1",
+    "url": "/natural_pic/static/js/runtime~main.789582d5.js"
+  },
+  {
+    "revision": "003b857e8dce28feee00",
+    "url": "/natural_pic/static/css/2.043c83a1.chunk.css"
+  },
+  {
+    "revision": "003b857e8dce28feee00",
+    "url": "/natural_pic/static/js/2.3995d08d.chunk.js"
+  },
+  {
+    "revision": "51c210460dfd9545db19e331cbd10807",
+    "url": "/natural_pic/static/media/fondo.51c21046.png"
+  },
+  {
+    "revision": "680b5d234e4f94c77be199d00b214a89",
+    "url": "/natural_pic/index.html"
+  }
+];
